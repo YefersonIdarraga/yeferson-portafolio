201436014446c87f3c62cd1db9b7919746cc4cf7
Yefer98 <yyefer98@gmail.com>
2023-09-17 17:07:25 -0500

    Cambio de tamaño video

--- src/components/projects/Project1.jsx
+++ src/components/projects/Project1.jsx
@@ -1,18 +1,60 @@
-import React,{ useState } from 'react'
+import React, { useState, useRef } from "react";
+import quoteVideo from "../../assets/quote-builder-video.mp4";
 
 const Project1 = ({ showInfo1, setMoreInfo1 }) => {
-    const handleClose = () => {
-        setMoreInfo1(false)
+  const videoRef = useRef(null);
+  const handleClose = () => {
+    setMoreInfo1(false);
+    if (videoRef.current) {
+      videoRef.current.pause();
     }
-  return (
-    <section className={`project-window ${showInfo1 ? "project-window-active" : ""}`}>
-        <i class="bi bi-x-circle" onClick={handleClose}></i>
-        <h2>Quote Builder</h2>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. At nam magnam accusantium eveniet consequatur harum! Esse accusamus voluptatum omnis nobis nulla, fuga dolorum impedit repellat asperiores, totam quasi, excepturi dolor.
-        </p>
-    </section>
-  )
-}
+  };
 
-export default Project1
+  const [video, setVideo] = useState(false);
+
+  const handleVideo = () => {
+    setVideo(true);
+  };
+
+  const handleVideoClose = () => {
+    setVideo(false);
+  };
+
+  return (
+    <section
+      className={`project-window ${showInfo1 ? "project-window-active" : ""}`}
+    >
+      <i class="bi bi-x-circle" onClick={handleClose}></i>
+      {!video && (
+        <>
+          <h2>Quote Builder</h2>
+          <p>
+            Fue un proyecto colaborativo desarrollado utilizando Android Studio
+            y programado en Java. Su función principal consiste en generar
+            cotizaciones a través de la selección de productos junto con sus
+            respectivas cantidades. Además de esta funcionalidad, el proyecto
+            cuenta con un sistema de autenticación que permite llevar un
+            registro de las cotizaciones realizadas, aprovechando las
+            herramientas proporcionadas por Firebase.<br></br> Durante la ejecución del
+            proyecto, mi principal responsabilidad se centró en establecer la
+            conexión con Firestore y crear los RecyclerViews, además de
+            encargarme de otros detalles importantes para su funcionamiento.
+          </p>
+        </>
+      )}
+      {video && (
+        <i class="bi bi-arrows-angle-contract" onClick={handleVideoClose}></i>
+      )}
+      <video
+        className={`video-default ${video ? "video" : ""}`}
+        controls
+        onClick={handleVideo}
+        ref={videoRef}
+      >
+        <source src={quoteVideo} type="video/mp4" />
+      </video>
+    </section>
+  );
+};
+
+export default Project1;
